ea765c5741430f7808bae56e44be4633913476e1
THmph <mph@thtelemedic.com>
2024-05-15 12:58:50 +0630

    add some form validation

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -9,6 +9,7 @@ use App\Models\Product;
 use Filament\Forms;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
@@ -19,6 +20,7 @@ use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Validation\Rule;
 
 class ProductResource extends Resource
 {
@@ -30,20 +32,43 @@ class ProductResource extends Resource
     {
         return $form
             ->schema([
-                TextInput::make('name')->required(),
-                TextInput::make('product_code')->required(),
+                // Section::make()->schema([
+                    
+                // ])
+                TextInput::make('name')->rules('min:3')->required(),
+
+                TextInput::make('product_code')
+                ->rules(function ($livewire) {
+                    $rules = ['required', 'string', 'max:255'];
+
+                    if ($livewire instanceof \Filament\Resources\Pages\CreateRecord) {
+                        $rules[] = Rule::unique('products', 'product_code');
+                    }
+
+                    return $rules;
+                })->required(),
+
                 Select::make('category_id')
                 ->label('Category')
                 ->options(Category::all()->pluck('name', 'id'))
-                ->searchable(),
+                ->searchable()
+                ->rules('required', 'exists:categories,id'),
+
                 Select::make('size')
                 ->options([
                     'small' => 'small',
                     'medium' => 'medium',
                     'large' => 'large',
                 ])->required(),
+
                 MarkdownEditor::make('description')->required(),
-                FileUpload::make('thumbnail')->label('Product Image')->disk('public')->directory('thumbnail')->required(),
+                
+                FileUpload::make('thumbnail')->label('Product Image')->disk('public')->directory('thumbnail')
+                    ->rules('required', 'image', 'max:2048'),
+            ])->columns([
+                'default'=>1,
+                'md'=>2,
+                'lg'=>2,
             ]);
     }
 
